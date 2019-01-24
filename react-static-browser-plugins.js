4d0d90f46930c79221edb75406c6e72e65c12f09
Michael Smith <michaelmilessmith@gmail.com>
2019-01-24 12:35:40 +0000

    Adds new site design

---
+++ react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/michaelsmith/playground/external-techs/node_modules/react-static-plugin-styled-components",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/michaelsmith/playground/external-techs",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
---
+++ templates/styles.7cd69add.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{113:function(n,w,o){}}]);
+//# sourceMappingURL=styles.7cd69add.js.map
